260dd1ba6830d2305540089174c3b8b049adcbcf
Sachin Boniface <36558@cgs.act.edu.au>
2015-07-27 09:28:00 +1000

    Implemented awesome feature

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -18,11 +18,11 @@ class ViewController: UIViewController {
         println("Hello World")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func awesomeFeature() {
+        
+        println("This is an awesome feature!");
+        
     }
-
 
 }
 
